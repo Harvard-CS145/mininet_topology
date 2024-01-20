af895182017b7d475eb085e59c1f033ec1bf1a9a
ChonLam Lao <laochanlam@gmail.com>
2024-01-20 00:46:36 -0500

    Update pull_update.sh
    Updates pull_update.sh to make it more robust

--- pull_update.sh
+++ pull_update.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-git remote add upstream "https://github.com/Harvard-CS145/mininet_topology.git"
+res=$(git remote -v | grep upstream)
+
+if [ -z "$res" ]
+then
+    # Depends on whether you want to use https or ssh link to clone
+    git remote add upstream "https://github.com/Harvard-CS145/mininet_topology.git"
+    # git remote add upstream "git@github.com:Harvard-CS145//mininet_topology.git"
+fi
+
 git pull upstream main --allow-unrelated-histories
